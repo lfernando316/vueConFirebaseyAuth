5517be6f4cecf45394c64697d4b9c6ab7720c456
lfernando316 <luiz_fernando316@hotmail.com>
2023-06-12 22:12:13 -0500

    vue con firebase y usando atenticacion de usuario

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>App</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Register</router-link> |
+      <button @click="userStore.logoutUser">Logout</button>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "./stores/user";
+const userStore = useUserStore();
+</script>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7yIEF7pVIm5Sb_Fy8DhIN7fDBv_f-Zyg",
+  authDomain: "vue-3-app-d90a8.firebaseapp.com",
+  projectId: "vue-3-app-d90a8",
+  storageBucket: "vue-3-app-d90a8.appspot.com",
+  messagingSenderId: "757063998413",
+  appId: "1:757063998413:web:56d49cc5c25d310a864284",
+};
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+export { auth };
